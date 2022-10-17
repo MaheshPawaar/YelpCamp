58f6fd03aa19e8e23b9a9ae5ee1ec690df3c222e
Mahesh Pawar <pawarmahesh723@gmail.com>
2022-10-17 18:06:49 +0530

    Seeding done

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
-mongoose.connect(dbUrl, {
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
