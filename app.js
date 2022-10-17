bd529d38afb484de58a4306ee2b8cd42962572c4
Mahesh Pawar <pawarmahesh723@gmail.com>
2022-10-17 18:04:40 +0530

    Update app

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex:true,
     useUnifiedTopology: true
 })
 
